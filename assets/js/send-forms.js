ef2fbb8a0ac76765a7b57fe0f1dd0d78034a42a6
Lia5 <Lia_na5@mail.ru>
2017-12-02 20:56:17 +0200

    3 - form

--- assets/js/send-forms.js
+++ assets/js/send-forms.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+  if(jQuery('#application-form').length) { 
       //E-mail Ajax Send
       $("#application-form form").submit(function() { //Change
         var th = $(this);
@@ -16,11 +16,32 @@ $(document).ready(function() {
         });
         return false;
       });
+    }
+      //E-mail Ajax Send
+    if(jQuery('#notification-form').length) {
+      $("#notification-form form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+          type: "POST",
+          url: "../../mail.php", //Change
+          data: th.serialize()
+        }).done(function() {
+          alert("Мы вас оповестим, если цена на услугу будет снижена!");
+          setTimeout(function() {
+            // Done Functions
+            th.trigger("reset");
+          }, 1000);
+        });
+        return false;
+      });
+    }
     
     });
+
     //select-number form
 
-    
+if(jQuery('.phone-mask').length) {
   jQuery(function($){
     $(".phone-mask").mask("+7(999) 999-9999");
   });
+}
