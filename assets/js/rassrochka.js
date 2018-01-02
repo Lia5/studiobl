744568c8fa565b91bcaffab5ef4349aee402b4fc
Lia5 <Lia_na5@mail.ru>
2018-01-02 09:03:08 +0200

    page halva

---
+++ assets/js/rassrochka.js
@@ -0,0 +1,11 @@
+
+
+$('.minus').click(function() {
+    $(this).parent().siblings().toggleClass('rassrochka__close');
+    $(this).siblings().toggleClass('plus-open');
+});
+  
+  $('.plus').click(function() {
+    $(this).parent().siblings().removeClass('rassrochka__close');
+    $(this).removeClass('plus-open');
+  });
