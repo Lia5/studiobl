1b3927fd10263339f0b87b61257e3239ba913009
Lia5 <Lia_na5@mail.ru>
2018-01-04 14:14:15 +0200

    js

--- assets/js/rassrochka.js
+++
@@ -1,7 +0,0 @@
-
-
-$('.tarifs__title').click(function() {
-    $(this).children('.plus').toggleClass('plus-open');
-    $(this).siblings('.block-on-off').toggleClass('rassrochka__close');
-});
-  
---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// JavaScript Document
+$(document).ready(function() {
+   
+	$(".contacts .one_third:first-child").click(function() {
+		if($(this).hasClass('active')){ }
+		else {
+			$(this).addClass("active");
+			$("#map").css("display","block");
+			$("#map2").css("display","none");
+			$("#map3").css("display","none");
+			$(".contacts .one_third:nth-child(2)").removeClass('active');
+    		$(".contacts .one_third:nth-child(3)").removeClass('active');
+		}
+	});
+	
+	$(".contacts .one_third:nth-child(2)").click(function() {
+		if($(this).hasClass('active')){ }
+		else {
+			$(this).addClass("active");
+			$("#map2").css("display","block");
+			$("#map").css("display","none");
+			$("#map3").css("display","none");
+			$(".contacts .one_third:first-child").removeClass('active');
+    		$(".contacts .one_third:nth-child(3)").removeClass('active');
+		}
+	});
+	
+	$(".contacts .one_third:nth-child(3)").click(function() {
+		if($(this).hasClass('active')){ }
+		else {
+			$(this).addClass("active");
+			$("#map3").css("display","block");
+			$("#map2").css("display","none");
+			$("#map").css("display","none");
+			$(".contacts .one_third:nth-child(2)").removeClass('active');
+    		$(".contacts .one_third:first-child").removeClass('active');
+		}
+	});	
+	
+	
+if(jQuery('.tarifs__title').length) {
+    $('.tarifs__title').click(function() {
+        $(this).children('.plus').toggleClass('plus-open');
+        $(this).siblings('.block-on-off').toggleClass('rassrochka__close');
+    });
+}
+
+});
